dc4d5c42df194385859b46084891921796296c2a
Ashutosh Varma <ashutoshvarma11@live.com>
2020-03-13 06:20:36 +0530

    add complete cmake build

--- external/external.cmake
+++ external/external.cmake
@@ -11,9 +11,13 @@ set(PNG_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/external/libpng)
 set(FREETYPE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/external/freetype2)
 
 add_subdirectory(${ZLIB_DIRECTORY})
-mimick_find("ZLIB" zlibstatic)
+set(ZLIB_LIB zlibstatic)
 
+# PNG requires ZLIB
+mimick_find("ZLIB" ${ZLIB_LIB})
 add_subdirectory(${PNG_DIRECTORY})
+set(PNG_LIB png_static)
+
 
 # Disbale all the freetype2 optional dependencies
 set(CMAKE_DISABLE_FIND_PACKAGE_HarfBuzz TRUE)
@@ -21,6 +25,14 @@ set(CMAKE_DISABLE_FIND_PACKAGE_BZip2 TRUE)
 set(CMAKE_DISABLE_FIND_PACKAGE_ZLIB TRUE)
 set(CMAKE_DISABLE_FIND_PACKAGE_PNG TRUE)
 unset_mimick_find("ZLIB")
+
 add_subdirectory(${FREETYPE_DIRECTORY})
+set(FREETYPE_LIB freetype)
+
+mimick_find("ZLIB" ${ZLIB_LIB})
+mimick_find("PNG" ${PNG_LIB})
+mimick_find("FREETYPE" ${FREETYPE_LIB})
+
+
 
 
