b2a3325b7fdf6e79650ff7d1c442d39cb1656e26
Ashutosh Varma <ashutoshvarma11@live.com>
2020-03-13 05:46:56 +0530

    add more variables to fix libraries which use _LIBRARIES & _DIRS

--- cmake/mimick_find.cmake
+++ cmake/mimick_find.cmake
@@ -6,14 +6,18 @@
 function(mimick_find LIB_NAME LIB)
     set(${LIB_NAME}_FOUND True PARENT_SCOPE)
     set(${LIB_NAME}_LIBRARY ${LIB} PARENT_SCOPE)
+    set(${LIB_NAME}_LIBRARIES ${LIB} PARENT_SCOPE)
 
     # Get the include dirs
     get_target_property(I_DIRS ${LIB} INCLUDE_DIRECTORIES)
     set(${LIB_NAME}_INCLUDE_DIR ${I_DIRS} PARENT_SCOPE)
+    set(${LIB_NAME}_INCLUDE_DIRS ${I_DIRS} PARENT_SCOPE)
 endfunction()
 
 function(unset_mimick_find LIB_NAME)
     unset(${LIB_NAME}_FOUND PARENT_SCOPE)
     unset(${LIB_NAME}_LIBRARY PARENT_SCOPE)
+    unset(${LIB_NAME}_LIBRARIES PARENT_SCOPE)
     unset(${LIB_NAME}_INCLUDE_DIR PARENT_SCOPE)
+    unset(${LIB_NAME}_INCLUDE_DIRS PARENT_SCOPE)
 endfunction()
